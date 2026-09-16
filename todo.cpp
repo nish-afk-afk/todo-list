ab7058e993039b613cd1d61b550ac9ffcd61b4fa
nishank <nishank_3@outlook.com>
2026-09-16 18:03:46 +0530

    todolist v0.1

--- todo.cpp
+++ todo.cpp
@@ -2,8 +2,23 @@
 #include <string>
 #include <vector>
 ////////////////////////////////////////////////////above is #includes
+int menu() {
+    std::cout << "\n" << "\n"
+              << "=====TODO=====\n"
+              << "user, please choose a option\n"
+              << "\n" << "\n"
+              << "1. view tasks\n"
+              << "2. add a task\n"
+              << "3. remove a task\n";
 
+    int choice;
+    std::cout << "\n\n"
+              << "please choose a option number:";
+    std::cin >> choice;
 
+    return choice;
+}
+//////////////////////////////////
 
 
 
@@ -14,9 +29,17 @@
 ///////////////////////////////////////////////////above is functions
 int main(){
 
-std::vector<std::string> tasks;
+    std::vector<std::string> tasks;
+
+    int chosen = menu();
 
 
+    std::cout << "you chose: " << chosen << "\n";
+
+
+
+
+    return 0;
 }
 
 
